918a376c6b7c0205c1d537c51051d2b573d668fb
Serge Tsygankov <baza@yandex-team.ru>
2015-12-21 02:12:20 +0300

    refactoring - separation into common module

---
+++ js/bazart.js
@@ -0,0 +1,80 @@
+/**
+ * Created by baza on 20/12/15.
+ */
+
+
+BAZART = (function($) {
+    var m = {
+        canvas: {},
+        artObject: null
+    };
+
+    m._patchJQuery = function() {
+        $.fn.center = function () {
+            this.css("position", "relative");
+            this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
+            $(window).scrollTop()) + "px");
+            this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
+            $(window).scrollLeft()) + "px");
+
+            return this;
+        };
+    };
+
+    m._initCanvas = function() {
+        if (($(window).width() / this.canvas.ratio.x < $(window).height() / this.canvas.ratio.y)) {
+            this.artObject.width($(window).width() * 0.9);
+            this.artObject.height(this.artObject.width() * (this.canvas.ratio.y / this.canvas.ratio.x));
+        } else {
+            this.artObject.height($(window).height() * 0.9);
+            this.artObject.width(this.artObject.height() * (this.canvas.ratio.x / this.canvas.ratio.y));
+        }
+
+        this.artObject.center();
+        // refresh canvas scale
+        var w = this.artObject.width(),
+            h = this.artObject.height();
+
+        this.ctx.canvas.width  = w;
+        this.ctx.canvas.height = h;
+
+        if (this.debug) {
+            var i;
+            // vertical ruler
+            for (i = 0; i <= this.canvas.ratio.x; i++) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(i * w / this.canvas.ratio.x, 0);
+                this.ctx.lineTo(i * w / this.canvas.ratio.x, h);
+                this.ctx.stroke();
+            }
+
+            // horizontal
+            for (i = 0; i <= this.canvas.ratio.y; i++) {
+                this.ctx.beginPath();
+                this.ctx.moveTo(0, i * h / this.canvas.ratio.y);
+                this.ctx.lineTo(w, i * h / this.canvas.ratio.y);
+                this.ctx.stroke();
+            }
+
+        }
+    };
+
+    m.init = function(canvasRatio, debug) {
+        this.canvas.ratio = canvasRatio;
+        this.debug = debug == undefined ? false : debug;
+        this.artObject = $("#art-object");
+        this.ctx = document.getElementById('art-object').getContext('2d');
+
+        this._patchJQuery();
+        this._initCanvas();
+
+        $(window).resize(function() {
+            if (window.RT) clearTimeout(window.RT);
+            window.RT = setTimeout(function() {
+                this.location.reload(false); /* false to get page from cache */
+            }, 100);
+        });
+    };
+
+    return m;
+})(jQuery);
